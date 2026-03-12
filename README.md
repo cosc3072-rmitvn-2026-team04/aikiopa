72c5a45ae95ac2d61c08962da9743ffbfa3d72dc
miketvo <miketvo@outlook.com>
2026-03-12 07:57:08 +0700

    [main] Godot project setup

---
+++ README.md
@@ -0,0 +1,85 @@
+
+AIKIOPA
+=======
+
+COSC3072 Games Studio 1 Project, Team 04, RMIT Vietnam 2026.
+
+## 📦 Dependencies
+
+- Godot 4.6-stable
+- Godot Export Template 4.6.stable
+- Git
+- Windows 10 and above (x64)
+
+Make sure you have installed these dependencies before continuing.
+
+You might want to use a package manager. My recommendations are: [winget](https://github.com/microsoft/winget-cli), [chocolatey](https://chocolatey.org/), or [scoop](https://scoop.sh/).
+
+For Linux/Mac users, you are suggested to run a Windows virtual machine.
+
+**Note**: Our project targets Windows 10 and above (x64) PCs only, and our development environment is set up entirely around the Windows ecosystem.
+
+
+## 🧩 Project Structure
+
+```yaml
+📂.
+├── 📂.github/          # GitHub Actions workflows
+├── 📂.vscode/          # Visual Studio Code config
+├── 📂assets/           # Assets (art, music, sfx, etc.)
+├── 📂scenes/           # Scene files (`.tscn`)
+├── 📂scripts/          # GDScript files (`.gd`)
+├── 📂shaders/          # Shader files (`.gdshader`)
+├── 📂build/            # Export artifacts (`.exe`)
+├── .editorconfig
+├── .gitattributes
+├── .gitignore
+├── build.ps1           # Export/build automation script
+├── export_presets.cfg  # Godot export config
+├── icon.svg            # Project icon
+├── project.godot       # Godot project config
+├── LICENSE             # License information
+└── README.md           # This file
+```
+
+
+### Code Editor/IDE:
+
+Recommended plugins if you decide to use [Visual Studio Code](https://code.visualstudio.com/):
+- [godot-tools](https://marketplace.visualstudio.com/items?itemName=geequlim.godot-tools).
+- [Godot Files](https://marketplace.visualstudio.com/items?itemName=alfish.godot-files).
+
+
+## 🧰 Export, Testing, and Deploy
+
+To export and play the game locally for testing and release, run the following command from the root directory:
+
+- Release export:
+    ```shell
+    ./build.ps1 -c -m release
+    ```
+- Debug export:
+    ```shell
+    ./build.ps1 -c -m debug
+    ```
+
+You can find export output under `build/` directory. Artifact archive (`.zip` files) can be found in `artifact/` directory.
+
+To clean exported files, run the following command:
+
+```shell
+./build.ps1 -c
+
+```
+
+For further information, use:
+
+```shell
+./build.ps1 -h
+
+```
+
+
+## 📃 Coding Conventions
+
+We follow the [GDScript style guide](https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_styleguide.html).
---
+++ build.ps1
@@ -0,0 +1,101 @@
+#
+# Syntax: build.ps1 [Options] -m <build-mode>
+# Options:
+#   -m Specify build mode. Accepted values are 'release' and 'debug'.
+#   -c (Optional) Clean build folders.
+#   -h (Optional) Display help and exit.
+#
+
+[CmdletBinding()] param(
+    [string]$m,
+    [string]$e,
+    [switch]$c,
+    [switch]$h
+)
+
+if ($h.IsPresent) {
+    Write-Output "Syntax: build.ps1 [Options] -m <build-mode>"
+    Write-Output "Options:"
+    Write-Output "  -m Specify build mode. Accepted values are 'release' and 'debug'."
+    Write-Output "  -e (Optional) Exclude projects (regex pattern, case sensitive). Defaults '^template$'."
+    Write-Output "  -c (Optional) Clean build folders."
+    Write-Output "  -h (Optional) Display this help and exit."
+    exit 0
+}
+
+if ($c.IsPresent) {
+    Write-Host " =====[ CLEANING BUILD DIRS ]===== " -ForegroundColor Black -BackgroundColor Magenta
+    git clean -dxf -e ".godot"
+    Write-Host "[ DONE ]" -ForegroundColor Magenta
+}
+
+$repositoryPath = $PSScriptRoot
+$archiveFolderPath = Join-Path -Path $repositoryPath -ChildPath "artifact"
+switch ($m) {
+    "" {
+        Write-Host "Error: No build mode specified. Terminating." -ForegroundColor Yellow
+        exit 0
+    }
+    "debug" {
+        $archiveFolderPath = Join-Path -Path $repositoryPath -ChildPath "artifact/debug"
+        $godotExportFlag = "--export-debug"
+    }
+    "release" {
+        $archiveFolderPath = Join-Path -Path $repositoryPath -ChildPath "artifact/release"
+        $godotExportFlag = "--export-release"
+    }
+    Default {
+        Write-Host "Invalid build mode '$m'" -ForegroundColor Red
+        exit 1
+    }
+}
+
+Write-Host " =====[ GODOT PROJECT REPOSITORY INFORMATION ]===== " -ForegroundColor Black -BackgroundColor Yellow
+Write-Host "- Repository path: $repositoryPath" -ForegroundColor Yellow
+Write-Host "- Artifact path: $archiveFolderPath" -ForegroundColor Yellow
+
+$buildModeText = $m.ToUpper()
+Write-Host " =====[ EXPORTING PROJECT (MODE: $buildModeText) ]===== " -ForegroundColor Black -BackgroundColor Magenta
+
+Write-Host "Importing project..." -ForegroundColor Yellow
+godot --headless --import --path $repositoryPath --quit | Out-Default
+
+Write-Host "Exporting project..." -ForegroundColor Magenta
+$exportPresetsFile = Join-Path -Path $repositoryPath -ChildPath "export_presets.cfg"
+$exportPresetsCfg = Get-Content -Path $exportPresetsFile -Raw
+$exportPresets = ($exportPresetsCfg | Select-String -Pattern '\n\nname="(.*)"' -AllMatches).Matches | ForEach-Object {
+    $_.Groups[1].Value
+}
+$exportPaths = ($exportPresetsCfg | Select-String -Pattern '\nexport_path="(.*)"' -AllMatches).Matches | ForEach-Object {
+    $_.Groups[1].Value
+}
+
+foreach ($exportPreset in $exportPresets) {
+    $exportPath = $exportPaths
+    if ($exportPaths -is [System.Array]) {
+        $exportPath = $exportPaths[$exportPresets.IndexOf($exportPreset)]
+    }
+    $absoluteExportPath = Join-Path -Path $repositoryPath -ChildPath $exportPath
+    $exportDirectory = Split-Path -Path $absoluteExportPath -Parent
+    if (-not (Test-Path -Path $exportDirectory)) {
+        New-Item -ItemType Directory -Path $exportDirectory | Out-Null
+    }
+
+    Write-Host "Exporting project ($exportPreset) ..." -ForegroundColor Yellow
+    godot --headless --path $repositoryPath $godotExportFlag $exportPreset | Out-Default
+
+    $zipFileName = "$exportPreset.zip"
+    $zipFilePath = Join-Path -Path $archiveFolderPath -ChildPath $zipFileName
+    if (-not (Test-Path -Path $archiveFolderPath)) {
+        New-Item -ItemType Directory -Path $archiveFolderPath | Out-Null
+    }
+    if (Test-Path -Path $zipFilePath) {
+        Remove-Item -Path $zipFilePath -Force
+    }
+    Write-Host "Archiving project ($exportPreset) from $exportDirectory into $zipFilePath" -ForegroundColor Yellow
+    Write-Host "- Source: $exportDirectory" -ForegroundColor Yellow
+    Write-Host "- Target: $zipFilePath" -ForegroundColor Yellow
+    Invoke-Expression -Command "zip -jr -0 $zipFilePath $exportDirectory"
+}
+
+Write-Host "[ DONE ]" -ForegroundColor Black -BackgroundColor Magenta
