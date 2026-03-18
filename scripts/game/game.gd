73e577f2c7b59a534a3125261e4192f3a9334c44
miketvo <miketvo@outlook.com>
2026-03-19 04:14:07 +0700

    [3] Added Game scene and its TerrainTileMapLayer

---
+++ scripts/game/game.gd
@@ -0,0 +1,13 @@
+class_name Game
+extends Node2D
+## Responsible for the main gameplay loop
+
+
+# ============================================================================ #
+#region Godot builtins
+
+func _ready() -> void:
+	pass
+
+#endregion
+# ============================================================================ #
