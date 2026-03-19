45d79fe40580e3e6c3dc2f30dc33759387750151
Tuong-Minh (Mike) Vo <miketvo@outlook.com>
2026-03-19 16:03:48 +0700

    Fix typos

--- scripts/modals/game_over_ui.gd
+++ scripts/modals/game_over_ui.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	%QuitToMenuButton.pressed.connect(_on_quit_to_menu_button_press)
 
 func _on_save_button_pressed() -> void:
-	acted.emit(&"save")
+	acted.emit(&"save_expedition")
 
 func _on_new_game_button_pressed() -> void:
 	acted.emit(&"new_game")
