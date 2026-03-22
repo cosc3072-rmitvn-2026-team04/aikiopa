08d21f253ee7a99b58ecf1833ff7b331979702c0
miketvo <miketvo@outlook.com>
2026-03-22 20:55:42 +0700

    [3] Added accomodation for game modes (tutorial vs. free play)

--- scripts/game/game.gd
+++ scripts/game/game.gd
@@ -2,6 +2,14 @@ extends Node2D
 ## Responsible for the main gameplay loop
 
 
+enum GameModes {
+	TUTORIAL,
+	FREE_PLAY,
+}
+
+@export var game_mode: GameModes = GameModes.FREE_PLAY
+
+
 # ============================================================================ #
 #region Godot builtins
 
