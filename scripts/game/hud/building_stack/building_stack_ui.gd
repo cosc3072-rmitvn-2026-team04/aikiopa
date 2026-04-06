6071cb811ede4313ae59972c4223e447379789a7
miketvo <miketvo@outlook.com>
2026-04-06 16:15:05 +0700

    [86] Updated BuildingStack HUD internal structure

---
+++ scripts/game/hud/building_stack/building_stack_ui.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+@export var curve: Curve
+@export var card_vertical_separation: int = 0
+
+var _building_card_scene = preload("res://scenes/game/hud/building_stack/building_card.tscn")
