4a02655c62851b23ae1ebf2639770a158ea45044
yoantran <s3927023@rmit.edu.vn>
2026-04-07 15:21:23 +0700

    [6] completed func set_type in building_card.gd

--- scripts/game/hud/building_stack/building_card.gd
+++ scripts/game/hud/building_stack/building_card.gd
@@ -6,6 +6,40 @@ func get_size() -> Vector2i:
 	return $CardBackgroundSprite2D.get_rect().size
 
 
+## Change type for CardForegroundSprite2D, BuildingSprite2D, BuildingNameLabel
 func set_type(_building: World.BuildingType) -> void:
-	# TODO: Implement logic here.
-	push_error("Not implemented")
+	const _FGPATH = "res://assets/building_stack/building_card/"
+	const _BUILDINGPATH = "res://assets/objects/"
+	match _building:
+		World.BuildingType.HOUSING:
+			$CardForegroundSprite2D.texture = load(_FGPATH + "housing_fg.png")
+			$BuildingSprite2D.texture = load(_BUILDINGPATH + "building_housing.png")
+			$BuildingNameLabel.text = "House"
+		World.BuildingType.SOLAR_FARM:
+			$CardForegroundSprite2D.texture = load(_FGPATH + "energy_fg.png")
+			$BuildingSprite2D.texture = load(_BUILDINGPATH + "building_solar_farm.png")
+			$BuildingNameLabel.text = "Solar Farm"
+		World.BuildingType.WIND_FARM:
+			$CardForegroundSprite2D.texture = load(_FGPATH + "energy_fg.png")
+			$BuildingSprite2D.texture = load(_BUILDINGPATH + "building_wind_farm.png")
+			$BuildingNameLabel.text = "Wind Farm"
+		World.BuildingType.NUCLEAR_REACTOR:
+			$CardForegroundSprite2D.texture = load(_FGPATH + "energy_fg.png")
+			$BuildingSprite2D.texture = load(_BUILDINGPATH + "building_nuclear_reactor.png")
+			$BuildingNameLabel.text = "Nuclear Reactor"
+		World.BuildingType.GREENHOUSE:
+			$CardForegroundSprite2D.texture = load(_FGPATH + "food_fg.png")
+			$BuildingSprite2D.texture = load(_BUILDINGPATH + "building_greenhouse.png")
+			$BuildingNameLabel.text = "Greenhouse"
+		World.BuildingType.RANCH:
+			$CardForegroundSprite2D.texture = load(_FGPATH + "food_fg.png")
+			$BuildingSprite2D.texture = load(_BUILDINGPATH + "building_ranch.png")
+			$BuildingNameLabel.text = "Ranch"
+		World.BuildingType.FISHERY:
+			$CardForegroundSprite2D.texture = load(_FGPATH + "food_fg.png")
+			$BuildingSprite2D.texture = load(_BUILDINGPATH + "building_fishery.png")
+			$BuildingNameLabel.text = "Fishery"
+		World.BuildingType.FACTORY:
+			$CardForegroundSprite2D.texture = load(_FGPATH + "industry_fg.png")
+			$BuildingSprite2D.texture = load(_BUILDINGPATH + "building_factory.png")
+			$BuildingNameLabel.text = "Factory"
