90687e3f41325f626b70d0927848389685304398
yoantran <s3927023@rmit.edu.vn>
2026-04-22 17:53:10 +0700

    [25] finished connecting splash screen with main menu

--- scripts/scenes/splash_screen.gd
+++ scripts/scenes/splash_screen.gd
@@ -1,22 +1,20 @@
 extends GameScene2D
 
 
-# @onread -> use $ or %
-
-var _animation_player: AnimationPlayer = %Sprite2D.AnimationPlayer
+@onready var _animation_player: AnimationPlayer = %AnimationPlayer
 
 
 # ============================================================================ #
 #region Godot builtins
 
 func _ready() -> void:
-    _animation_player.connect("animation_finished", _end_splash.unbind(1))
-    _animation_player.play("default") # change default ->...
+	_animation_player.connect("animation_finished", _end_splash.unbind(1))
+	_animation_player.play("splash")
 
 
 func _process(_delta: float) -> void:
-    if Input.is_action_pressed("ui_cancel"):
-        _end_splash()
+	if Input.is_action_pressed("ui_cancel"):
+		_end_splash()
 
 #endregion
 # ============================================================================ #
@@ -26,8 +24,8 @@ func _process(_delta: float) -> void:
 #region Private methods
 
 func _end_splash() -> void:
-    _animation_player.stop(true)
-    scene_finished.emit(SceneKey.MAIN_MENU)
+	_animation_player.stop(true)
+	scene_finished.emit(SceneKey.MAIN_MENU)
 
 #endregion
 # ============================================================================ #
