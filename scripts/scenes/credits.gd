e39bb8ec2489fab8036a92afcf21b77c807a91c3
miketvo <miketvo@outlook.com>
2026-05-01 14:58:30 +0700

    [142] Fixed manual scroll logic in Credits scene

--- scripts/scenes/credits.gd
+++ scripts/scenes/credits.gd
@@ -5,10 +5,11 @@ extends GameScene2D
 # ============================================================================ #
 #region Exported properties
 
-@export_range(10.0, 100.0, 0.1, "suffix:px/s") var auto_scroll_speed: float = 60.0
-@export_range(10.0, 100.0, 0.1, "suffix:px/s") var input_scroll_speed: float = 400.0
-@export_range(0.0, 5.0, 0.1, "suffix:s") var scroll_restart_delay: float = 1.5
-@export var scroll_paused: bool = false
+@export_range(10.0, 100.0, 0.1, "or_greater", "suffix:px/s")
+var auto_scroll_speed: float = 10.0
+
+@export_range(10.0, 1000.0, 0.1, "or_greater", "suffix:px/s")
+var manual_scroll_speed: float = 10.0
 
 #endregion
 # ============================================================================ #
@@ -17,8 +18,8 @@ extends GameScene2D
 # ============================================================================ #
 #region Private variables
 
-var _scroll_resart_timer: Timer = Timer.new()
 var _current_scroll_position: float = 0.0
+var _scroll_paused: bool = false
 
 #endregion
 # ============================================================================ #
@@ -30,10 +31,7 @@ var _current_scroll_position: float = 0.0
 func _ready() -> void:
 	_scene_transition_in()
 
-	# Scroll restart Timer.
-	add_child(_scroll_resart_timer)
-	_scroll_resart_timer.one_shot = true
-	_scroll_resart_timer.timeout.connect(_on_scroll_restart_timer_timeout)
+	%ScrollRestartTimer.timeout.connect(_on_scroll_restart_timer_timeout)
 
 	visibility_changed.connect(_on_visibility_changed)
 	%ScrollContainer.scroll_started.connect(_on_scroll_container_scroll_started)
@@ -41,26 +39,26 @@ func _ready() -> void:
 	%ScrollContainer.resized.connect(_on_scroll_container_resized)
 
 	_set_header_and_footer()
-	scroll_paused = false
+	_scroll_paused = false
 
 
 func _process(delta: float) -> void:
-	if not visible or scroll_paused:
+	if not visible:
 		return
 
 	var input_axis = Input.get_axis("ui_up", "ui_down")
 	if input_axis != 0:
 		# Manual scrolling.
-		_apply_scroll(input_axis * input_scroll_speed * delta)
+		_apply_scroll(input_axis * manual_scroll_speed * delta)
 		_start_scroll_restart_timer()
-	else:
+	elif not _scroll_paused:
 		# Auto scrolling.
 		_apply_scroll(auto_scroll_speed * delta)
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		scroll_paused = true
+		_scroll_paused = true
 		MainMenu.scene_transition_in_enabled = false
 		await _scene_transition_out()
 		scene_finished.emit(SceneKey.MAIN_MENU)
@@ -83,27 +81,20 @@ func _scene_transition_out() -> void:
 
 
 func _start_scroll_restart_timer() -> void:
-	scroll_paused = true
-	_scroll_resart_timer.start(scroll_restart_delay)
+	_scroll_paused = true
+	%ScrollRestartTimer.start()
 
 
 func _apply_scroll(amount: float) -> void:
 	_current_scroll_position += amount
 
-	# Calculate scroll distance.
-	var content_height = %ScrollContainer.get_v_scroll_bar().max_value
-	var max_scroll = content_height - %ScrollContainer.size.y
-
-	# Clamp to keep values within bounds.
-	_current_scroll_position = clamp(_current_scroll_position, 0, max_scroll)
+	var content_height: float = %ScrollContainer.get_v_scroll_bar().max_value
+	var max_scroll: float = content_height - %ScrollContainer.size.y
+	_current_scroll_position = clampf(_current_scroll_position, 0.0, max_scroll)
 	%ScrollContainer.scroll_vertical = int(_current_scroll_position)
 
 	if _current_scroll_position >= max_scroll and max_scroll > 0:
-		_end_reached()
-
-
-func _end_reached() -> void:
-	scroll_paused = true
+		_scroll_paused = true
 
 
 func _set_header_and_footer() -> void:
@@ -122,7 +113,7 @@ func _set_header_and_footer() -> void:
 func _on_scroll_restart_timer_timeout() -> void:
 	# Sync tracker with the actual UI position.
 	_current_scroll_position = float(%ScrollContainer.scroll_vertical)
-	scroll_paused = false
+	_scroll_paused = false
 
 
 # Listens to visibility_changed.
@@ -130,8 +121,8 @@ func _on_visibility_changed() -> void:
 	if visible:
 		_current_scroll_position = 0.0
 		%ScrollContainer.scroll_vertical = 0
-		scroll_paused = false
 		%RichTextLabel.grab_focus()
+		_scroll_paused = false
 
 
 # Listens to %ScrollContainer.scroll_started.
@@ -144,14 +135,18 @@ func _on_scroll_container_scroll_started() -> void:
 func _on_scroll_container_gui_input(event: InputEvent) -> void:
 	# Pause auto-scroll if mouse wheel is used.
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP or event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+		if (
+				event.button_index == MOUSE_BUTTON_WHEEL_UP
+				or event.button_index == MOUSE_BUTTON_WHEEL_DOWN
+		):
+			_current_scroll_position = float(%ScrollContainer.scroll_vertical)
 			_start_scroll_restart_timer()
 
 
 # Listens to %ScrollContainer.resized.
 func _on_scroll_container_resized() -> void:
-	_set_header_and_footer()
 	_current_scroll_position = float(%ScrollContainer.scroll_vertical)
+	_set_header_and_footer()
 
 #endregion
 # ============================================================================ #
